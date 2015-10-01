2439ffaa8c9b7bbe071fe121592cb37ee40fb6f8
sunandag <rcsunanda@gmail.com>
2015-09-30 18:20:40 -0700

    Edit #3 to class2.h by git_learn_1:master

--- lib/class2.h
+++ lib/class2.h
@@ -8,3 +8,10 @@
 //This edit was done after restoring this file from a previous commit (d197c8e)
 //newline3
 //***********************************************
+
+
+//***********************************************
+Edit 3: by git_learn_1:master
+newline: This edit is needed by git_learn_2:feature_test5
+//***********************************************
+
