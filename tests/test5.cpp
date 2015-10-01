217c69eb8e6d36d250229328eb343047fc79656b
sunandag <rcsunanda@gmail.com>
2015-09-30 18:57:28 -0700

    Edit #3 to test5.cpp by git_learn_2:feature_test5 after core lib update by git_learn_1

--- tests/test5.cpp
+++ tests/test5.cpp
@@ -9,3 +9,9 @@ Edit: 2 - after this edit git_learn_1 will change master's core lib
 newline2
 //***********************************************
 
+
+//***********************************************
+Edit: 3 - now feature_test5 has merged with master
+and got the core lib updates (to class 1 and class 2
+done by git_learn_1 in the master branch
+//***********************************************
