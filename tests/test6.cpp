cb36d5d000cc2566e88b2d50025183b5b4d36c5a
sunandag <rcsunanda@gmail.com>
2015-10-02 18:24:07 -0700

    Edit #2 to test6.cpp by git_learn_2

--- tests/test6.cpp
+++ tests/test6.cpp
@@ -1,4 +1,10 @@
 //***********************************************
-Edit 1: (creation) of test6.cpp - Mary
+Edit 1: (creation) of test6.cpp by git_learn_2
 This file is a separate feature run by Mary
+//***********************************************
+
+
+//***********************************************
+Edit 2: git_learn_2 keeps working
+Meanwhile git_learn_1 updates the core library and pushes to central rep
 //***********************************************
