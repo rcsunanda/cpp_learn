f2c4a2b1a809ea79b025b5402913c2855df86d10
sunandag <rcsunanda@gmail.com>
2015-10-02 18:28:21 -0700

    Edit #2 (done) to class3.h by git_learn_1

--- lib/class3.h
+++ lib/class3.h
@@ -3,3 +3,10 @@ Edit: 1 (creation) of class3.h by git_learn_1
 This is an update to the core lib
 //***********************************************
 
+
+//***********************************************
+Edit: 2
+git_learn_1 is finished with the lib update and now pushes to central repo
+This will put him ahead of git_learn_2 on master branch
+Which means git_learn_2 has to pull and rebase the remote master before pushing his code
+//***********************************************
