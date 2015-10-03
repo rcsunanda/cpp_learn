060c737a7eb0c488d60b7c05aae41ad474d32907
sunandag <rcsunanda@gmail.com>
2015-10-02 18:25:35 -0700

    Edit #1 (creation) to class3.h by git_learn_1

---
+++ lib/class3.h
@@ -0,0 +1,5 @@
+//***********************************************
+Edit: 1 (creation) of class3.h by git_learn_1
+This is an update to the core lib
+//***********************************************
+
