9ccabff859849c43174c830b99fb3657d3182cde
sunandag <rcsunanda@gmail.com>
2015-10-02 18:22:03 -0700

    Edit #1 (create) to test6.cpp - git_learn_2

---
+++ tests/test6.cpp
@@ -0,0 +1,4 @@
+//***********************************************
+Edit 1: (creation) of test6.cpp - Mary
+This file is a separate feature run by Mary
+//***********************************************
